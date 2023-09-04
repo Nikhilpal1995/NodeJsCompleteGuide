cabfe093fe7f4b57c038be55af335d5496ffb847
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-09-04 21:22:19 +0530

    Studnt object creation

--- arrow.js
+++ arrow.js
@@ -2,3 +2,16 @@
 const prod1 = (a,b) => a*b;
 
 console.log(prod1(2,3));
+
+//create a student object
+
+const studnt {
+    name: Nikhil
+    age: 28
+    const greet{
+        "Hi, my name is " + this.name + "my age is " + this.age;
+    }
+    greet;
+}
+
+console.log(studnt);

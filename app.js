e5a9043f6a9ef44a4a6a4f48e61ae2cf3f9189f0
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-09-11 23:19:55 +0530

    Req & Response

--- app.js
+++ app.js
@@ -7,7 +7,33 @@ const http = require('http');
 //http.createServer(rqlistener);
 
 const server = http.createServer((req, res) => {
-    console.log(`my name is NIKHIL`);
-})
+    console.log(req.url, req.method, req.headers);
 
-server.listen(4000);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My Node.js Server</title></head>');
+    //res.write(`<body><h1>Hello from my Node.js Server!</h1></body>`);
+    res.write('<body>');
+
+    // Use a switch statement to handle different URLs
+    switch (req.url) {
+        case '/home':
+            res.write('<h1>Welcome home</h1>');
+            break;
+        case '/about':
+            res.write('<h1>Welcome to About Us page</h1>');
+            break;
+        case '/node':
+            res.write('<h1>Welcome to my Node.js project</h1>');
+            break;
+        default:
+            res.write('<h1>My Node.js project</h1>');
+            break;
+    }
+
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);

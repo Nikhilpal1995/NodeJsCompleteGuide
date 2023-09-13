00185907fdf876aaa3847dcf28cbd6666c74b654
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-09-13 21:44:22 +0530

    Project Demo

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
+
+    fs.readFile("message.txt", { encoding: "utf-8"}, (err, data) => {
+      if(err){
+        console.log(err);
+      }
+      console.log(`data from file` + data);
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title><head>');
+      res.write(`<body>${data}</body>`);
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      res.write('</html>');
+      return res.end();
+    });
+    
   }
-  if (url === '/message' && method === 'POST') {
+  else if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
       console.log(chunk);
-      body.push(chunk);
+      body.push(chunk); //[array]
     });
     return req.on(`end`, () =>{
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
+      const parsedBody = Buffer.concat(body).toString(); 
+      const message = parsedBody.split('=')[1]; // ['message', 'my name is yas']
       fs.writeFileSync('message.txt', message, (err) => {
+        if(err){
+          console.log(err);
+        }
+        console.log(`inside fs.writeFile`);
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();

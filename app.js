c7133e12b5d940e1a97885ca12ff20afa20beca2
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-09-15 21:31:46 +0530

    clean up the code using Node Modules System

--- app.js
+++ app.js
@@ -1,52 +1,10 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require('./routes');
 
-  if (url === '/') {
+console.log(routes.someText);
 
-    fs.readFile("message.txt", { encoding: "utf-8"}, (err, data) => {
-      if(err){
-        console.log(err);
-      }
-      console.log(`data from file` + data);
-      res.write('<html>');
-      res.write('<head><title>Enter Message</title><head>');
-      res.write(`<body>${data}</body>`);
-      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-      res.write('</html>');
-      return res.end();
-    });
-    
-  }
-  else if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk); //[array]
-    });
-    return req.on(`end`, () =>{
-      const parsedBody = Buffer.concat(body).toString(); 
-      const message = parsedBody.split('=')[1]; // ['message', 'my name is yas']
-      fs.writeFileSync('message.txt', message, (err) => {
-        if(err){
-          console.log(err);
-        }
-        console.log(`inside fs.writeFile`);
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });      
-    });  
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
-
+//const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
+ 
 server.listen(3000);

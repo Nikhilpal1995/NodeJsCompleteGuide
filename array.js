3b0d743a2d0d7d89217940ea7cb9a88d012f1ed3
Nikhilpal1995 <nikhilpal1720@gmail.com>
2023-09-26 12:25:29 +0530

    Routers & Filters

--- array.js
+++
@@ -1,7 +0,0 @@
-// Transform array into ['apple', 'oranges' , 'empty string', 'mango', 'empty string', 'lemon] using array maps
-
-const arr = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
-
-const transarr = arr.map(item => item === ' '? 'emprty string': item);
-
-console.log(transarr);
